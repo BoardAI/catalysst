538478d3fbc81f0bdcd13687d86f8c7861172907
Victor Ndaba <mwangindaba@gmail.com>
2024-12-17 18:47:08 +0300

    ci: use `infra` folder

--- sst.config.ts
+++ sst.config.ts
@@ -10,30 +10,6 @@ export default $config({
     };
   },
   async run() {
-    const fn = new sst.aws.Function("WebhooksHandler", {
-      handler: "src/index.handler",
-      environment: {
-        APP_ID: process.env.APP_ID!,
-        PRIVATE_KEY: process.env.PRIVATE_KEY!,
-        WEBHOOK_SECRET: process.env.WEBHOOK_SECRET!,
-      },
-      nodejs: {
-        install: [
-          "probot",
-          "@probot/adapter-aws-lambda-serverless",
-          "date-fns",
-        ],
-        format: "cjs",
-      },
-      memory: "2048 MB",
-    });
-
-    const api = new sst.aws.ApiGatewayV2("WebhooksApi");
-
-    api.route("POST /{proxy+}", fn.arn);
-
-    return {
-      endpoint: api.url,
-    };
+    await import("./infra");
   },
 });
